0dc903db36052cdf00765b83739eae208e52c03e
Jesse Paulsen <jesseraypaulsen@gmail.com>
2022-03-21 16:29:19 -0500

    second commit

---
+++ Page.js
@@ -0,0 +1,18 @@
+export function Page() {
+  return (
+    <div>
+      <h3>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+        exercitationem? Possimus, eligendi? Cupiditate impedit hic expedita
+        incidunt! Aspernatur fugit ipsam rem excepturi eligendi quos, minima
+        vero! Iste praesentium suscipit molestiae?
+      </h3>
+      <h4>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+        exercitationem? Possimus, eligendi? Cupiditate impedit hic expedita
+        incidunt! Aspernatur fugit ipsam rem excepturi eligendi quos, minima
+        vero! Iste praesentium suscipit molestiae?
+      </h4>
+    </div>
+  );
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import { Page } from "./Page";
+import "./styles.css";
+
+const container = document.getElementById("container");
+ReactDOM.render(<Page />, container);
