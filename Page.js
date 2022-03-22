371c288667ed8c39c260d8776404198e8b2b9214
Jesse Paulsen <jesseraypaulsen@gmail.com>
2022-03-22 16:43:51 -0500

    ....

--- Page.js
+++ Page.js
@@ -5,29 +5,22 @@ import { Contact } from "./Contact";
 import { Project } from "./Project";
 import { Routes, Route, Link } from "react-router-dom";
 
+{
+  /*  
+  <Route exact path="/" element={<Work />}>
+    <Work />
+  </Route> */
+}
 export function Page() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Work />}>
-          <Work />
-        </Route>
+        <Route exact path="/" element={<Work />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
         <Route path="project" element={<Project />} />
       </Routes>
     </div>
   );
-}
-
-{
-  /*
-   return (
-     <div>
-       <Header />
-       <Project />
-     </div>
-   );
-   */
 }
