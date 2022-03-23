f2983bf46648533e9f848a9f6f1febdc6960dfe2
Jesse Paulsen <jesseraypaulsen@gmail.com>
2022-03-23 08:35:35 -0500

    all other Material scripts go in Contact.js

--- Contact.js
+++ Contact.js
@@ -1,54 +1,90 @@
+import { useEffect, useRef } from "react";
+import { MDCTextField } from "@material/textfield";
+import { MDCTextFieldCharacterCounter } from "@material/textfield/character-counter";
+import { MDCRipple } from "@material/ripple";
+
 export function Contact() {
+  const nameField = useRef(null);
+  const emailField = useRef(null);
+  const messageField = useRef(null);
+  const characterCounter = useRef(null);
+  const button = useRef(null);
+  useEffect(() => {
+    new MDCTextField(nameField.current);
+    new MDCTextField(emailField.current);
+    new MDCTextField(messageField.current);
+    new MDCTextFieldCharacterCounter(characterCounter.current);
+    new MDCRipple(button.current);
+  }, []);
+
   return (
-    <main class="contactPage">
-      <h1 class="mdc-typography--headline2 centered">Contact Me</h1>
-      <p class="contactText">
+    <main className="contactPage">
+      <h1 className="mdc-typography--headline2 centered">Contact Me</h1>
+      <p className="contactText">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
         cupiditate, pariatur autem deserunt numquam dignissimos amet fuga cumque
         possimus maiores quaerat, alias rem, commodi esse culpa molestiae ut
         aspernatur.
       </p>
-      <form class="contactForm">
-        <div class="mdc-text-field mdc-text-field--name">
-          <input type="text" id="my-name-field" class="mdc-text-field__input" />
-          <label class="mdc-floating-label" htmlFor="my-name-field">
+      <form className="contactForm">
+        <div className="mdc-text-field mdc-text-field--name" ref={nameField}>
+          <input
+            type="text"
+            id="my-name-field"
+            className="mdc-text-field__input"
+          />
+          <label className="mdc-floating-label" htmlFor="my-name-field">
             Name
           </label>
-          <div class="mdc-line-ripple"></div>
+          <div className="mdc-line-ripple"></div>
         </div>
-        <div class="mdc-text-field mdc-text-field--email">
+
+        <div className="mdc-text-field mdc-text-field--email" ref={emailField}>
           <input
             type="text"
             id="my-email-field"
-            class="mdc-text-field__input"
+            className="mdc-text-field__input"
           />
-          <label class="mdc-floating-label" htmlFor="my-email-field">
+          <label className="mdc-floating-label" htmlFor="my-email-field">
             Email
           </label>
-          <div class="mdc-line-ripple"></div>
+          <div className="mdc-line-ripple"></div>
         </div>
-        <div class="mdc-text-field mdc-text-field--textarea mdc-text-field--message">
-          <div class="mdc-text-field-character-counter">0 / 500</div>
+
+        <div
+          className="mdc-text-field mdc-text-field--textarea mdc-text-field--message"
+          ref={messageField}
+        >
+          <div
+            className="mdc-text-field-character-counter"
+            ref={characterCounter}
+          >
+            0 / 500
+          </div>
           <textarea
             id="textarea"
-            class="mdc-text-field__input"
+            className="mdc-text-field__input"
             rows="8"
             cols="40"
             maxLength="500"
           ></textarea>
-          <div class="mdc-notched-outline">
-            <div class="mdc-notched-outline__leading"></div>
-            <div class="mdc-notched-outline__notch">
-              <label htmlFor="textarea" class="mdc-floating-label">
+          <div className="mdc-notched-outline">
+            <div className="mdc-notched-outline__leading"></div>
+            <div className="mdc-notched-outline__notch">
+              <label htmlFor="textarea" className="mdc-floating-label">
                 Textarea Label
               </label>
             </div>
-            <div class="mdc-notched-outline__trailing"></div>
+            <div className="mdc-notched-outline__trailing"></div>
           </div>
         </div>
-        <button class="mdc-button mdc-button--raised submitButton">
-          <span class="mdc-button__label">Submit</span>
-          <i class="material-icons mdc-button__icon" aria-hidden="true">
+
+        <button
+          className="mdc-button mdc-button--raised submitButton"
+          ref={button}
+        >
+          <span className="mdc-button__label">Submit</span>
+          <i className="material-icons mdc-button__icon" aria-hidden="true">
             favorite
           </i>
         </button>
--- Work.js
+++ Work.js
@@ -6,7 +6,7 @@ export function Work() {
       <h1 className="mdc-typography--headline2 centered">Work</h1>
       <ul className="cardList">
         <li className="mdc-card card">
-          <Link to="/project" className="mdc-card__primary-action" tabindex="0">
+          <Link to="/project" className="mdc-card__primary-action" tabIndex="0">
             <img
               src="https://cdn.glitch.com/31357884-a11a-4a7b-9fe5-0322583e8062%2Fflowers3.jpg?1558559904507"
               alt=""
@@ -26,7 +26,7 @@ export function Work() {
           </Link>
         </li>
         <li className="mdc-card card">
-          <Link to="/project" className="mdc-card__primary-action" tabindex="0">
+          <Link to="/project" className="mdc-card__primary-action" tabIndex="0">
             <img
               src="https://cdn.glitch.com/31357884-a11a-4a7b-9fe5-0322583e8062%2Fflowers5.jpg?1558558462565"
               alt=""
@@ -46,7 +46,7 @@ export function Work() {
           </Link>
         </li>
         <li className="mdc-card card">
-          <Link to="/project" className="mdc-card__primary-action" tabindex="0">
+          <Link to="/project" className="mdc-card__primary-action" tabIndex="0">
             <img
               src="https://cdn.glitch.com/31357884-a11a-4a7b-9fe5-0322583e8062%2Fflowers4.jpg?1558558462683"
               alt=""
@@ -66,7 +66,7 @@ export function Work() {
           </Link>
         </li>
         <li className="mdc-card card">
-          <Link to="/project" className="mdc-card__primary-action" tabindex="0">
+          <Link to="/project" className="mdc-card__primary-action" tabIndex="0">
             <img
               src="https://cdn.glitch.com/31357884-a11a-4a7b-9fe5-0322583e8062%2Fflowers7.jpg?1558558462200"
               alt=""
